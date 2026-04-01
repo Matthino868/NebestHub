735be5d43cbe193abdbb2d0671b9a11ec2124d96
Vasileios Bomponis <vasileios.bomponis@nebest.nl>
2026-04-01 16:54:09 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import App from './App.tsx'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './assets/styles/index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>  
+  </StrictMode>,
+)
